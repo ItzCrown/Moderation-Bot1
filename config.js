0cf6c01da73ee75ed58ef10182cf020557fb7267
ItzCrown <76158889+ItzCrown@users.noreply.github.com>
2021-03-05 14:17:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `!`;//your prefix for bot
-exports.Token = `ODE3MzY0MTA1MjE1MjEzNTc5.YEIbpA.AoDno--H6kh6iJmrTc4lIdty3oc`;//your token 
-exports.Color = `#33FFD2`;//color   of embed
+exports.Prefix = `PREFIX_FOR_BOT`;//your prefix for bot
+exports.Token = `YOUR_BOT_TOKEN`;//your token 
+exports.Color = `YOUR_COLOR`;//color   of embed
